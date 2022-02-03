a8b25f3a3e859f4fd294f6ab75601a2705d0cbf1
janusha <janushas@dsny.com>
2022-02-03 18:49:59 -0500

    select a song in clone and it will play in spotify

--- components/Song.js
+++ components/Song.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { useRecoilState } from 'recoil'
 import useSpotify from '../hooks/useSpotify'
 import { millisToMinutesAndSeconds } from '../lib/time'
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
 
 function Song({ order, track }) {
   const spotifyApi = useSpotify()
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState)
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
 
+  const playSong = () => {
+    setCurrentTrackId(track.track.id)
+    setIsPlaying(true)
+    spotifyApi.play({
+      uris: [track.track.uri],
+    })
+  }
   return (
-    <div className="grid cursor-pointer grid-cols-2 rounded-lg py-4 px-5 text-gray-500 hover:bg-gray-900">
+    <div
+      className="grid cursor-pointer grid-cols-2 rounded-lg py-4 px-5 text-gray-500 hover:bg-gray-900"
+      onClick={playSong}
+    >
       <div className="flex items-center space-x-4 ">
         <p>{order + 1}</p>
         <img
